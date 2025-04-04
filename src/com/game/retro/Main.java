b7bfb3c62ded94f9fe5d360a4259e7e041135e5f
Vrissmok <cristian.cmma56@gmail.com>
2025-04-03 20:56:23 -0600

    Creación de clases

--- src/com/game/retro/Main.java
+++ src/com/game/retro/Main.java
@@ -1,4 +1,9 @@
 package com.game.retro;
 
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
 }
---
+++ src/com/game/retro/Tetris.java
@@ -0,0 +1,7 @@
+package com.game.retro;
+
+public class Tetris {
+
+    public final byte[][] table = new byte[12][22];
+
+}
